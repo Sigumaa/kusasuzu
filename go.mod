4cbea499d5a1417d28d724b855e6e114ac9b9100
Sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-11-25 22:38:42 +0900

    wip

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/Sigumaa/kusasuzu
 go 1.19
 
 require (
+	github.com/bwmarrin/discordgo v0.26.1 // indirect
 	github.com/g8rswimmer/go-twitter/v2 v2.1.4 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
+	golang.org/x/crypto v0.3.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,1 +1,77 @@
 package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"github.com/g8rswimmer/go-twitter/v2"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+type authorize struct {
+	Token string
+}
+
+func (a authorize) Add(req *http.Request) {
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", a.Token))
+}
+
+var metaBytes []byte
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	API_KEY := os.Getenv("API_KEY")
+	API_KEY_SECRET := os.Getenv("API_KEY_SECRET")
+	ACCESS_TOKEN := os.Getenv("ACCESS_TOKEN")
+	ACCESS_TOKEN_SECRET := os.Getenv("ACCESS_TOKEN_SECRET")
+	BEARER_TOKEN := os.Getenv("BEARER_TOKEN")
+	DISCORD_TOKEN := os.Getenv("DISCORD_TOKEN")
+	DISCORD_WEBHOOK := os.Getenv("DISCORD_WEBHOOK")
+	CHANNEL_ID := os.Getenv("CHANNEL_ID")
+	hoge := fmt.Sprintf("%s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n", API_KEY, API_KEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET, BEARER_TOKEN, DISCORD_TOKEN, DISCORD_WEBHOOK, CHANNEL_ID)
+	fmt.Println(hoge)
+
+	client := &twitter.Client{
+		Authorizer: authorize{
+			Token: BEARER_TOKEN,
+		},
+		Client: http.DefaultClient,
+		Host:   "https://api.twitter.com",
+	}
+	opts := twitter.TweetRecentSearchOpts{
+		Expansions:  []twitter.Expansion{twitter.ExpansionEntitiesMentionsUserName, twitter.ExpansionAuthorID},
+		TweetFields: []twitter.TweetField{twitter.TweetFieldCreatedAt, twitter.TweetFieldConversationID, twitter.TweetFieldAttachments},
+	}
+
+	fmt.Println("Callout to tweet recent search callout")
+
+	query := "草鈴 has:links -is:retweet"
+	tweetResponse, err := client.TweetRecentSearch(context.Background(), query, opts)
+	if err != nil {
+		log.Panicf("tweet lookup error: %v", err)
+	}
+
+	dictionaries := tweetResponse.Raw.TweetDictionaries()
+
+	enc, err := json.MarshalIndent(dictionaries, "", "    ")
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println(string(enc))
+
+	metaBytes, err = json.MarshalIndent(tweetResponse.Meta, "", "    ")
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println(string(metaBytes))
+}
